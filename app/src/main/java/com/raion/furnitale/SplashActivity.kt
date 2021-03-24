8a0c5c9708160a4e828614e1970e6de24d768075
KylixEza <loilyxtra@gmail.com>
2021-03-24 19:39:41 +0700

    update SplashActivity.kt

--- app/src/main/java/com/raion/furnitale/SplashActivity.kt
+++ app/src/main/java/com/raion/furnitale/SplashActivity.kt
@@ -1,11 +1,35 @@
 package com.raion.furnitale
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.raion.furnitale.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    companion object {
+        const val TIME_FOR_SPLASH = 2000L
+    }
+
+    private var _splashBinding: ActivitySplashBinding? = null
+    private val splashBinding get() = _splashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        _splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding?.root)
+
+        val splashHandler = Handler(mainLooper)
+
+        splashHandler.postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME_FOR_SPLASH)
+    }
+
+    override fun onDestroy() {
+        _splashBinding = null
+        super.onDestroy()
     }
 }
