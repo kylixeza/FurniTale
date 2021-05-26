b68b4185967e60ee6ca6676b58749dde49cf1670
Kylix <58837451+KylixEza@users.noreply.github.com>
2021-05-27 00:18:58 +0700

    add CategoryType.kt (Enum)

---
+++ app/src/main/java/com/raion/furnitale/core/utils/CategoryType.kt
@@ -0,0 +1,10 @@
+package com.raion.furnitale.core.utils
+
+enum class CategoryType(val desc: String) {
+    LIVINGROOM("Living Room"),
+    BEDROOM("Bedroom"),
+    KITCHEN("Kitchen"),
+    BATHROOM("Bathroom"),
+    OUTDOOR("Outdoor"),
+    ACCESSORIES("Accessories")
+}
